72cdef8a56d4687876cc2d4da07b27515bcfabcc
Persuasive Archives <mediapersuasive@gmail.com>
2025-05-28 10:58:10 +0800

    Create header.php

---
+++ templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Portable Video Host</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <a href="index.php">Portable Video Host</a>
+</header>
+<main>
