705cb91402adf0e21eb6a698d78a576e35f1cc32
Persuasive Archives <mediapersuasive@gmail.com>
2025-06-04 17:12:50 +0800

    added link to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer style="text-align: center; padding: 20px; background: #202020; color: white;">
-    &copy; <?= date('Y') ?> Portable Video Host
+    &copy; <?= date('Y') ?> Powered by <a href="https://github.com/PersuasiveArchives/PortableVideoHost/">Portable Video Host </a>
 </footer>
 </body>
 </html>
