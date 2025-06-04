6a02f60afe79d712b52419d1e203275c84067533
Persuasive Archives <mediapersuasive@gmail.com>
2025-06-04 16:59:15 +0800

    fixed space

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer style="text-align: center; padding: 20px; background: #202020; color: white;">
-    &copy; <?= date('Y') ?>Portable Video Host
+    &copy; <?= date('Y') ?> Portable Video Host
 </footer>
 </body>
 </html>
